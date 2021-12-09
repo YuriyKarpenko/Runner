92acf7aee4070028379f3dcbb9dcbea4c82963c1
Yuriy Karpenko <molodoy@li.ru>
2021-12-09 03:11:14 +0300

    runParameter

--- Runner/ViewModels/OptionParameterViewModel.cs
+++ Runner/ViewModels/OptionParameterViewModel.cs
@@ -14,61 +14,9 @@ namespace Runner.ViewModels
 		public OptionParameterViewModel(ParameterItem parameterItem)
 		{
 			ParameterItem = parameterItem;
-			InitCommands();
 		}
 		
 
 		public ParameterItem ParameterItem { get; }
-
-		#region action
-
-		public CommandWrapper CmdSelectFolder { get; private set; }
-
-		private void InitCommands()
-		{
-			var canSelect = this.ObservableForProperty(i => i.ParameterItem, p => p.ParameterType == ParameterType.FileMask);
-			//CmdSelectFolder = CommandWrapper.Create("...", ActSelectFolder, canSelect);
-		}
-
-			/*
-		private void ActSelectFolder()
-		{
-#if AVALONIA
-			var dialog = new Avalonia.Controls.OpenFolderDialog
-			{
-				Directory = Path.GetDirectoryName(ParameterItem.Value),
-				//Title = "Select a folder",
-			};
-
-			var folder = dialog.ShowAsync(new Avalonia.Controls.Window()).Result;
-			if (!string.IsNullOrEmpty(folder))
-			{
-				var mask = Path.GetFileName(ParameterItem.Value);
-				ParameterItem.Value = Path.Combine(folder, mask);
-				this.RaisePropertyChanged(nameof(ParameterItem));
-			}
-#else
-			var dialog = new Microsoft.Win32.SaveFileDialog
-			{
-				CheckFileExists = false,
-				CheckPathExists = true,
-				CreatePrompt = false,
-				InitialDirectory = Path.GetDirectoryName(ParameterItem.Value),
-				FileName = "select folder",
-				OverwritePrompt = false,
-				Title = "Select a folder",
-			};
-
-			if (dialog.ShowDialog() == true)
-			{
-				var folder = Path.GetDirectoryName(dialog.FileName);
-				var mask = Path.GetFileName(ParameterItem.Value);
-				ParameterItem.Value = Path.Combine(folder, mask);
-				this.RaisePropertyChanged(nameof(ParameterItem));
-			}
-#endif
-		}
-			//	*/
-		#endregion
 	}
 }
---
+++ Runner/ViewModels/RunParameterViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Reactive.Bindings;
+
+using ReactiveUI;
+
+using Runner.Models;
+
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Runner.ViewModels
+{
+    class RunParameterViewModel : VmBase
+    {
+        private readonly OptionItem _optionItem;
+        private readonly ParameterItem _parameterItem;
+
+        public RunParameterViewModel(OptionItem optionItem, ParameterItem parameterItem)
+        {
+            _optionItem = optionItem;
+            _parameterItem = parameterItem;
+
+            IsCombo = parameterItem.ParameterType == ParameterType.FileMask;
+            IsText = parameterItem.ParameterType == ParameterType.Const;
+
+            Value = parameterItem.ParameterType switch
+            {
+                ParameterType.Const => new ReactiveProperty<string?>(parameterItem.Value),
+                _ => new ReactiveProperty<string?>(),
+            };
+
+            IsValid = Value.Select(i => !string.IsNullOrEmpty(i)).ToReadOnlyReactiveProperty();
+
+            Refresh();
+        }
+
+
+        public string[]? List { get; private set; }
+        public bool IsCombo { get; }
+        public bool IsText { get; }
+        public IReactiveProperty<string?> Value { get; }
+        public IReadOnlyReactiveProperty<bool> IsValid { get; }
+
+
+        public void Refresh()
+        {
+            if (_parameterItem.ParameterType == ParameterType.FileMask)
+            {
+                var currDir = Path.GetDirectoryName(_optionItem.ProgramPath) ?? Directory.GetCurrentDirectory();
+                var hasDir = Directory.Exists(currDir);
+                if (hasDir)
+                {
+                    var mask = Path.GetFileName(_parameterItem.Value);
+                    List = Directory.GetFiles(currDir, mask).Select(AdjustParameter).ToArray();
+                    this.RaisePropertyChanged(nameof(List));
+                }
+
+                Value.Value = null;
+            }
+        }
+
+        private string AdjustParameter(string filePath)
+        {
+            var result = filePath;
+            if (!_parameterItem.IncludePath)
+            {
+                result = Path.GetFileName(result);
+            }
+            if (!_parameterItem.IncludeExtensions)
+            {
+                result = Path.GetFileNameWithoutExtension(result);
+            }
+            return result;
+        }
+    }
+}
